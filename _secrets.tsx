8160a25be99975442a5ff2f5b995504fca4cc090
C0nstant1n <c0nnstant1n.cs@gmail.com>
2023-09-26 00:05:56 +0700

    Update _secrets.tsx

--- _secrets.tsx
+++ _secrets.tsx
@@ -1,3 +1,5 @@
+//Это копия файла secrets.tsx, который должен находится в корне проекта,
+//тут указывается что в нем должно содержаться
 //ваш API-ключ (yandex GeoCoder https://yandex.ru/dev/maps/geocoder/)
 const key: string = "";
 //API OpenWeather key (https://openweathermap.org/current)
